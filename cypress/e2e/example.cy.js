48dad450101793f4fb0ac84cff27d761ec0984b0
Alan R <alan.ristic@gmail.com>
2024-10-01 12:36:59 +0200

    Test: Added E2E test example of playing audio

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,14 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
+/**
+ * TEST GROUP for E2E Testing
+ */
+describe('Sanity Test', () => {
+  /**
+   * TEST CASE 01: Visit 'home' page
+   */
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('#header a:first-child', 'Music') // Check if the text 'You did it!' is present on the rendered page
   })
 })
